cba9d8d11f8a385a830b2eed78e4cb490ca5e12e
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-14 14:15:48 -0700

    Update Image.php

--- app/Image.php
+++ app/Image.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $table = "images";
+    
     protected $fillable = ['image','es_id'];
+    
     public function estate(){
         return $this->belongsTo(\App\Es::class,'es','id');
     }
