bd12ff4d4189b2468c0e49b57c28189c497a02a6
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-14 14:16:41 -0700

    Update User.php

--- app/User.php
+++ app/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+        
         'name', 'email', 'password','admin',
     ];
 
@@ -25,6 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
+        
         'password', 'remember_token',
     ];
 
